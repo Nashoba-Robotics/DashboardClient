dd160034ccda82dd52454497d6c17f46991478e7
alexbrinister <alex_brinister@yahoo.com>
2013-10-24 22:44:31 -0400

    Put Apache Commons in a more appropriate place

---
+++ src/dashboardclient/DashboardClient.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dashboardclient;
+
+import org.apache.commons.net.telnet.TelnetClient;
+
+/**
+ *
+ * @author alexbrinister
+ */
+public class DashboardClient {
+    TelnetClient client = new TelnetClient();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
